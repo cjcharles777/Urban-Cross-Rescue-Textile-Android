dbca963269217650924934f155cfa20d6e80cc18
cjcharles777 <cjcharles777@gmail.com>
2014-03-19 17:24:37 -0500

    3/19/14 - Auto Complete improvement. - Open item: How to create a visual way of adding Target Portal. Answer: make it a list. - Added List view of portal - Issue: Can not get list to show more than one item. Needs Debugging.

---
+++ blackops/src/main/java/com/ucr/bravo/blackops/adapters/PortalSubmitArrayAdapter.java
@@ -0,0 +1,19 @@
+package com.ucr.bravo.blackops.adapters;
+
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+import com.ucr.bravo.blackops.rest.object.beans.Portal;
+
+/**
+ * Created by cedric on 3/19/14.
+ */
+public class PortalSubmitArrayAdapter  extends ArrayAdapter<Portal>
+{
+
+    public PortalSubmitArrayAdapter(Context context, int resource) {
+        super(context, resource);
+    }
+
+
+}
--- blackops/src/main/java/com/ucr/bravo/blackops/fragments/TargetSubmissionFragment.java
+++ blackops/src/main/java/com/ucr/bravo/blackops/fragments/TargetSubmissionFragment.java
@@ -1,12 +1,16 @@
 package com.ucr.bravo.blackops.fragments;
 
+import android.app.ListFragment;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.ucr.bravo.blackops.BlackOpsApplication;
@@ -14,12 +18,17 @@ import com.ucr.bravo.blackops.R;
 import com.ucr.bravo.blackops.adapters.PortalSearchArrayAdapter;
 import com.ucr.bravo.blackops.rest.object.beans.Portal;
 
+import java.util.ArrayList;
+
 /**
  * Created by cedric on 3/14/14.
  */
 public class TargetSubmissionFragment extends Fragment
 {
     private AutoCompleteTextView actv;
+    ArrayAdapter<String> adapter;
+    ArrayList<String> listItems = new ArrayList<String>();
+    Portal selected;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -36,9 +45,29 @@ public class TargetSubmissionFragment extends Fragment
             public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
                                     long arg3)
             {
-                Portal selected = (Portal) arg0.getAdapter().getItem(arg2);
+                selected = (Portal) arg0.getAdapter().getItem(arg2);
+                listItems.add(selected.getName());
+                adapter.notifyDataSetChanged();
             }
         });
+        adapter=new ArrayAdapter<String>(getActivity(),android.R.layout.simple_list_item_1,listItems);
+        ListView listview = (ListView) rootView.findViewById(android.R.id.list);
+        listview.setAdapter(adapter);
+
+        Button button = (Button) rootView.findViewById(R.id.addButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v)
+            {
+                if(selected != null)
+                {
+                    listItems.add(selected.getName());
+                    adapter.notifyDataSetChanged();
+                    actv.setText("");
+                    selected = null;
+                }
+            }
+        });
+
         return rootView;
     }
 
